034922c8b0744880b407bff56751bff8ebcbb086
krummitz <amidbidee@gmail.com>
2022-04-15 01:44:32 +0100

    added more documentation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,43 @@
 #!/usr/bin/python3
 """
 Lockboxes
+
+Problem definition:
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes.
+
+The Challenge is to figure out if all boxes[list]
+can be unlocked, if so return True else False
+
+Prototype: def canUnlockAll(boxes)
+- boxes is a list of lists
+- A key with the same number as a box opens that box
+- assume all keys will be positive integers
+- There can be keys that do not have boxes
+- The first box boxes[0] is unlocked
+- Return True if all boxes can be opened, else return False
+
+Algorithm:
+    functions: [
+        unlock: checks if a box key is available, by
+                matching it's key with a list of available key,
+                if it's unlockable, unlock it and take the items[int]
+                in it.
+
+        canUnlockAll: checks if the list of boxes can all be unlocked,
+            it calls unlock on each of box in the list, if
+            it was unlocked, moves to next box without doing nothing,
+            else it logs the locked box in a list of locked boxes.
+            if this list remains unoccupied at the end of it's opertion,
+            it returns a False, else a True.
+    ]
 """
-import copy
 
 
 def unlock(keys: list, box: dict) -> bool:
     """
-    tries to unlocks a box with available keys
+    tries to unlock a box with available keys
     if it unlocks collect items of box as new keys
 
     Args:
@@ -17,6 +47,8 @@ def unlock(keys: list, box: dict) -> bool:
 
     Returns: bool
     """
+    # takes a key[int] and checks if
+    # if matches the box key
     for key in keys:
         if key == box['key']:
             for item in box['items']:
@@ -44,23 +76,28 @@ def canUnlockAll(boxes: list) -> bool:
 
     unlock(available_keys, box)
 
+    # first traversal on list of boxes
+    # it updates the box object with current box
+    # passes the box object into unlock()
+    # if box was successully unlocked, do nothing
+    # else add it to a list of locked boxes
     for i in range(1, len(boxes)):
         u_box = {'key': i, 'items': boxes[i]}
         box.update(**u_box)
-        # print(f"box: {box['key']}, available_keys: {available_keys}")
         unlocked = unlock(available_keys, box)
-        # print(u_box)
+
         if unlocked:
             pass
         else:
             locked.append(u_box)
 
-    # print(available_keys)
+    # if locked list is not empty
+    # travers the list, and repeat similar
+    # if no boxes are left unlocked return to main function
+    # else return False
     if locked:
         still_locked = []
         for i in range(len(locked)):
-            # print(f"box: {locked[i]['key']},
-            # available_keys: {available_keys}")
             unlocked = unlock(available_keys, locked[i])
             if unlocked:
                 pass
