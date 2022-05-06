dc1deca9cc443a6323526cd9d9829e3fea9c43e1
krummitz <amidbidee@gmail.com>
2022-05-06 03:39:08 +0100

    validate utf8 function

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+validate UTF-8
+"""
+
+
+def validUTF8(data):
+    """
+    checks if a list og integers are
+    valid utf-8 codepoints
+    """
+
+    seek = 0
+    n = len(data)
+    for i in range(n):
+        if seek > 0:
+            seek -= 1
+            continue
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
+            return False
+        elif data[i] <= 0x7f:
+            seek = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            # 4-byte utf-8 character encoding
+            span = 4
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                seek = span - 1
+            else:
+                return False
+        elif data[i] & 0b11110000 == 0b11100000:
+            # 3-byte utf-8 character encoding
+            span = 3
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                seek = span - 1
+            else:
+                return False
+        elif data[i] & 0b11100000 == 0b11000000:
+            # 2-byte utf-8 character encoding
+            span = 2
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                seek = span - 1
+            else:
+                return False
+        else:
+            return False
+    return True
